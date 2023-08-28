aee2f8dc7611957e193b1f0c4b8faf00837099ab
Tanishq2807 <117887119+Tanishq2807@users.noreply.github.com>
2023-08-28 21:33:54 +0530

    Update PID.ino
    Updated the code with void setup and some minor corrections (defining some undefined variables)

--- PID.ino
+++ PID.ino
@@ -1,19 +1,32 @@
 #include <PID_v1.h>
 #include <Servo.h>
 
-// Define PID gains
+//Define PID gains  
 double kp = 1.0;
 double ki = 0.1;
 double kd = 0.2;
 
-double setpoint = 0.0; // Desired trajectory value
+double setpoint = 0.0;
 double input_x = 0.0;
 double input_z = 0.0;
+double output_x = 0.0;
+double output_z = 0.0;
 
-void loop()
-{
-    double angle_x = readAngleX;
-    double angle_z = readAngleZ;
+Servo servoX;
+Servo servoZ;
+
+void setup() {
+    servoX.attach(/*X servo pin*/);
+    servoZ.attach(/*Z servo pin*/);
+}
+
+PID myPID_x(&input_x, &output_x, &setpoint, kp, ki, kd, DIRECT);
+PID myPID_z(&input_z, &output_z, &setpoint, kp, ki, kd, DIRECT);
+
+
+void loop() {
+    double angle_x = readAngleX(); // Replace with actual sensor reading
+    double angle_z = readAngleZ(); // Replace with actual sensor reading
 
     input_x = angle_x;
     input_z = angle_z;
@@ -26,14 +39,13 @@ void loop()
 
     delay(10);
 }
-PID myPID_x(&input_x, &output_x, &setpoint, kp, ki, kd, DIRECT);
-PID myPID_z(&input_z, &output_z, &setpoint, kp, ki, kd, DIRECT);
 
-void adjustServoX(double control_x){
+void adjustServoX(double control_x) {
     // double servoAngle = map(control_x, -255, 255, 0, 180);
     servoX.write(servoAngle);
 }
-void adjustServoY(double control_z){
+
+void adjustServoZ(double control_z) {
     // double servoAngle = map(control_z, -255, 255, 0, 180);
     servoZ.write(servoAngle);
 }
