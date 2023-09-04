04ed4061c0f74ce9c24314edae99e352b7f44caa
advayb <17509808+advayb@users.noreply.github.com>
2023-09-04 23:27:53 +0530

    created file
    tested MPU 6500 library, implemented kalman filter for roll and pitch. Have to implement filter on other variables and calculate velocity from acceleration.

---
+++ mpu_6500_kalman_test.ino/mpu_6500_kalman_test.ino.ino
@@ -0,0 +1,300 @@
+/***************************************************************************
+* Example sketch for the MPU6500_WE library
+*
+* This sketch shows how to get acceleration, gyroscocope and temperature 
+* data from the MPU6500. In essence, the difference to the MPU9250 is the
+* missing magnetometer. The shall only show how to "translate" all other 
+* MPU9250 example sketches for use of the MPU6500
+* 
+* For further information visit my blog:
+*
+* https://wolles-elektronikkiste.de/mpu9250-9-achsen-sensormodul-teil-1  (German)
+* https://wolles-elektronikkiste.de/en/mpu9250-9-axis-sensor-module-part-1  (English)
+* 
+***************************************************************************/
+
+#include <MPU6500_WE.h>
+#include <Wire.h>
+#include "Kalman.h"
+#define MPU6500_ADDR 0x68
+
+xyzFloat gValue;
+xyzFloat gyr;
+xyzFloat angle;
+Kalman kalmanX; // Create the Kalman instances
+Kalman kalmanY;
+int checkFlag;
+double accX, accY, accZ;
+double gyroX, gyroY, gyroZ;
+int16_t tempRaw;
+double gyroXangle, gyroYangle; // Angle calculate using the gyro only
+double compAngleX, compAngleY; // Calculated angle using a complementary filter
+double kalAngleX, kalAngleY; // Calculated angle using a Kalman filter
+uint32_t timer;
+
+/* There are several ways to create your MPU6500 object:
+ * MPU6500_WE myMPU6500 = MPU6500_WE()              -> uses Wire / I2C Address = 0x68
+ * MPU6500_WE myMPU6500 = MPU6500_WE(MPU6500_ADDR)  -> uses Wire / MPU6500_ADDR
+ * MPU6500_WE myMPU6500 = MPU6500_WE(&wire2)        -> uses the TwoWire object wire2 / MPU6500_ADDR
+ * MPU6500_WE myMPU6500 = MPU6500_WE(&wire2, MPU6500_ADDR) -> all together
+ * Successfully tested with two I2C busses on an ESP32
+ */
+MPU6500_WE myMPU6500 = MPU6500_WE(MPU6500_ADDR);
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  if(!myMPU6500.init()){
+    Serial.println("MPU6500 does not respond");
+  }
+  else{
+    Serial.println("MPU6500 is connected");
+  }
+  
+  /* The slope of the curve of acceleration vs measured values fits quite well to the theoretical 
+   * values, e.g. 16384 units/g in the +/- 2g range. But the starting point, if you position the 
+   * MPU6500 flat, is not necessarily 0g/0g/1g for x/y/z. The autoOffset function measures offset 
+   * values. It assumes your MPU6500 is positioned flat with its x,y-plane. The more you deviate 
+   * from this, the less accurate will be your results.
+   * The function also measures the offset of the gyroscope data. The gyroscope offset does not   
+   * depend on the positioning.
+   * This function needs to be called at the beginning since it can overwrite your settings!
+   */
+  Serial.println("Position you MPU6500 flat and don't move it - calibrating...");
+  delay(1000);
+  myMPU6500.autoOffsets();
+  Serial.println("Done!");
+  
+  /*  This is a more accurate method for calibration. You have to determine the minimum and maximum 
+   *  raw acceleration values of the axes determined in the range +/- 2 g. 
+   *  You call the function as follows: setAccOffsets(xMin,xMax,yMin,yMax,zMin,zMax);
+   *  Use either autoOffset or setAccOffsets, not both.
+   */
+  //myMPU6500.setAccOffsets(-14240.0, 18220.0, -17280.0, 15590.0, -20930.0, 12080.0);
+
+  /*  The gyroscope data is not zero, even if you don't move the MPU6500. 
+   *  To start at zero, you can apply offset values. These are the gyroscope raw values you obtain
+   *  using the +/- 250 degrees/s range. 
+   *  Use either autoOffset or setGyrOffsets, not both.
+   */
+  //myMPU6500.setGyrOffsets(45.0, 145.0, -105.0);
+
+  /*  You can enable or disable the digital low pass filter (DLPF). If you disable the DLPF, you 
+   *  need to select the bandwdith, which can be either 8800 or 3600 Hz. 8800 Hz has a shorter delay,
+   *  but higher noise level. If DLPF is disabled, the output rate is 32 kHz.
+   *  MPU6500_BW_WO_DLPF_3600 
+   *  MPU6500_BW_WO_DLPF_8800
+   */
+  myMPU6500.enableGyrDLPF();
+  //myMPU6500.disableGyrDLPF(MPU6500_BW_WO_DLPF_8800); // bandwdith without DLPF
+  
+  /*  Digital Low Pass Filter for the gyroscope must be enabled to choose the level. 
+   *  MPU6500_DPLF_0, MPU6500_DPLF_2, ...... MPU6500_DPLF_7 
+   *  
+   *  DLPF    Bandwidth [Hz]   Delay [ms]   Output Rate [kHz]
+   *    0         250            0.97             8
+   *    1         184            2.9              1
+   *    2          92            3.9              1
+   *    3          41            5.9              1
+   *    4          20            9.9              1
+   *    5          10           17.85             1
+   *    6           5           33.48             1
+   *    7        3600            0.17             8
+   *    
+   *    You achieve lowest noise using level 6  
+   */
+  myMPU6500.setGyrDLPF(MPU6500_DLPF_6);
+
+  /*  Sample rate divider divides the output rate of the gyroscope and accelerometer.
+   *  Sample rate = Internal sample rate / (1 + divider) 
+   *  It can only be applied if the corresponding DLPF is enabled and 0<DLPF<7!
+   *  Divider is a number 0...255
+   */
+  myMPU6500.setSampleRateDivider(5);
+
+  /*  MPU6500_GYRO_RANGE_250       250 degrees per second (default)
+   *  MPU6500_GYRO_RANGE_500       500 degrees per second
+   *  MPU6500_GYRO_RANGE_1000     1000 degrees per second
+   *  MPU6500_GYRO_RANGE_2000     2000 degrees per second
+   */
+  myMPU6500.setGyrRange(MPU6500_GYRO_RANGE_250);
+
+  /*  MPU6500_ACC_RANGE_2G      2 g   (default)
+   *  MPU6500_ACC_RANGE_4G      4 g
+   *  MPU6500_ACC_RANGE_8G      8 g   
+   *  MPU6500_ACC_RANGE_16G    16 g
+   */
+  myMPU6500.setAccRange(MPU6500_ACC_RANGE_2G);
+
+  /*  Enable/disable the digital low pass filter for the accelerometer 
+   *  If disabled the bandwidth is 1.13 kHz, delay is 0.75 ms, output rate is 4 kHz
+   */
+  myMPU6500.enableAccDLPF(true);
+
+  /*  Digital low pass filter (DLPF) for the accelerometer, if enabled 
+   *  MPU6500_DPLF_0, MPU6500_DPLF_2, ...... MPU6500_DPLF_7 
+   *   DLPF     Bandwidth [Hz]      Delay [ms]    Output rate [kHz]
+   *     0           460               1.94           1
+   *     1           184               5.80           1
+   *     2            92               7.80           1
+   *     3            41              11.80           1
+   *     4            20              19.80           1
+   *     5            10              35.70           1
+   *     6             5              66.96           1
+   *     7           460               1.94           1
+   */
+  myMPU6500.setAccDLPF(MPU6500_DLPF_6);
+
+  /* You can enable or disable the axes for gyroscope and/or accelerometer measurements.
+   * By default all axes are enabled. Parameters are:  
+   * MPU6500_ENABLE_XYZ  //all axes are enabled (default)
+   * MPU6500_ENABLE_XY0  // X, Y enabled, Z disabled
+   * MPU6500_ENABLE_X0Z   
+   * MPU6500_ENABLE_X00
+   * MPU6500_ENABLE_0YZ
+   * MPU6500_ENABLE_0Y0
+   * MPU6500_ENABLE_00Z
+   * MPU6500_ENABLE_000  // all axes disabled
+   */
+  //myMPU6500.enableAccAxes(MPU6500_ENABLE_XYZ);
+  //myMPU6500.enableGyrAxes(MPU6500_ENABLE_XYZ);
+  delay(200);
+
+  float t = millis();
+  /* Set kalman and gyro starting angle */
+  Serial.println("Please enter 1 when ready");
+  while (!Serial.available()) {
+          ; // Wait for user input
+        }
+  checkFlag = Serial.parseInt();
+  if(checkFlag ==1){
+    gValue = myMPU6500.getGValues();
+
+    accX = gValue.x;
+    accY = gValue.y;
+    accZ = gValue.z;
+    // double roll  = atan2(accY, accZ) * RAD_TO_DEG; //check about this!!!!!!!!!
+    // double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+    double roll = myMPU6500.getRoll();
+    double pitch = myMPU6500.getPitch();
+    kalmanX.setAngle(roll); // Set starting angle
+    kalmanY.setAngle(pitch);
+    gyroXangle = roll;
+    gyroYangle = pitch;
+    compAngleX = roll;
+    compAngleY = pitch;
+    timer = micros();
+  }
+  else{
+    Serial.println("Incorrect setup");
+  }
+}
+
+void loop() {
+  gValue = myMPU6500.getGValues();
+  gyr = myMPU6500.getGyrValues();
+  angle = myMPU6500.getAngles();
+
+  float temp = myMPU6500.getTemperature();
+  float resultantG = myMPU6500.getResultantG(gValue);
+  float pitch = myMPU6500.getPitch();
+  float roll  = myMPU6500.getRoll();
+
+  accX = gValue.x;
+  accY = gValue.y;
+  accZ = gValue.z;
+  
+  //gyr is in deg/s while we want gyro in rad/s
+  gyroX = gyr.x;
+  gyroY = gyr.y;
+  gyroZ = gyr.z;
+
+  double dt = (double)(micros() - timer) / 1000000; // Calculate delta time
+  timer = micros();
+  //double roll  = atan2(accY, accZ); // * RAD_TO_DEG;
+  //double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)); //* RAD_TO_DEG;
+  double gyroXrate = gyroX; // / 131.0; // Convert to deg/s
+  double gyroYrate = gyroY; // / 131.0; // Convert to deg/s
+  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+  if ((roll < -90 && kalAngleX > 90) || (roll > 90 && kalAngleX < -90)) {
+    kalmanX.setAngle(roll);
+    compAngleX = roll;
+    kalAngleX = roll;
+    gyroXangle = roll;
+  } else
+    kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+  if (abs(kalAngleX) > 90)
+    gyroYrate = -gyroYrate; // Invert rate, so it fits the restriced accelerometer reading
+  kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt);
+  gyroXangle += gyroXrate * dt; // Calculate gyro angle without any filter
+  gyroYangle += gyroYrate * dt;
+  //gyroXangle += kalmanX.getRate() * dt; // Calculate gyro angle using the unbiased rate
+  //gyroYangle += kalmanY.getRate() * dt;
+  compAngleX = 0.9 * (compAngleX + gyroXrate * dt) + 0.1 * roll; // Calculate the angle using a Complimentary filter
+  compAngleY = 0.9 * (compAngleY + gyroYrate * dt) + 0.1 * pitch;
+  // Reset the gyro angle when it has drifted too much
+  if (gyroXangle < -180 || gyroXangle > 180)
+    gyroXangle = kalAngleX;
+  if (gyroYangle < -180 || gyroYangle > 180)
+    gyroYangle = kalAngleY;
+  // Serial.print(accX); Serial.print(":");
+  // Serial.print(accY); Serial.print(":");
+  // Serial.print(accZ); Serial.print(":");
+  Serial.print(roll); Serial.print(" ");
+  Serial.print(pitch); Serial.print(" \n");
+  // Serial.print(gyroXangle); Serial.print(":");
+  // Serial.print(gyroYangle); Serial.print(":");
+  // Serial.print(compAngleX); Serial.print(":");
+  // Serial.print(compAngleY); Serial.print(":");
+  // Serial.print(kalAngleX); Serial.print(":");
+  // Serial.println(kalAngleY);// Serial.print("\t");
+  //Serial.print("\r\n");
+
+  printData(gValue, gyr, angle, pitch, roll, resultantG, temp, kalAngleX, kalAngleY);
+
+  delay(200);
+
+
+
+}
+
+void printData (xyzFloat gValue, xyzFloat gyr, xyzFloat angle, float pitch, float roll, float resultantG, float temp, double KangX, double KangY){
+  Serial.println("Acceleration in g (x,y,z):");
+  Serial.print(gValue.x);
+  Serial.print("   ");
+  Serial.print(gValue.y);
+  Serial.print("   ");
+  Serial.println(gValue.z);
+  Serial.print("Resultant g: ");
+  Serial.println(resultantG);
+
+  Serial.print("Angle x  = ");
+  Serial.print(KangX);
+  Serial.print("  |  Angle y  = ");
+  Serial.print(KangY);
+  // Serial.print("  |  Angle z  = ");
+  // Serial.println(angle.z);
+
+  Serial.print("Orientation of the module: ");
+  Serial.println(myMPU6500.getOrientationAsString());
+
+  Serial.print("Pitch   = "); 
+  Serial.print(pitch); 
+  Serial.print("  |  Roll    = "); 
+  Serial.println(roll);
+
+  Serial.println();
+  Serial.println("Gyroscope data in degrees/s: ");
+  Serial.print(gyr.x);
+  Serial.print("   ");
+  Serial.print(gyr.y);
+  Serial.print("   ");
+  Serial.println(gyr.z);
+
+  Serial.print("Temperature in °C: ");
+  Serial.println(temp);
+
+  Serial.println("********************************************");
+
+  
+}
